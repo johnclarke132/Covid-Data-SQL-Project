3a1421cea0c4ed3701d416fbf9af261836c333a0
john132 <52978533+johnclarke132@users.noreply.github.com>
2022-07-31 19:15:47 +0200

    Clean up & comments

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,6 +1,7 @@
 -- View CovidDeaths Data
 SELECT Location, date, total_cases, new_cases, total_deaths, population
 	FROM [Covid Data SQL Project]..CovidDeaths$
+	-- Ommit grouped and class data
 	WHERE continent IS NOT NULL
 	ORDER BY 1,2
 
@@ -9,32 +10,32 @@ SELECT Location, date, total_cases, new_cases, total_deaths, population
  	FROM [Covid Data SQL Project]..CovidVaccinations$
 	WHERE continent IS NOT NULL
 
--- Total Cases vs Total Deaths (Mortality rate)
+-- Mortality rate by day (Total Cases vs Total Deaths)
 SELECT Location, date, total_cases, total_deaths, (total_deaths/total_cases) * 100 AS Percentage
 	FROM [Covid Data SQL Project]..CovidDeaths$
 	WHERE continent IS NOT NULL
 	ORDER BY 1,2
 
--- Total Cases vs Total Deaths (Mortality rate - UK)
+-- Mortality rate by day - UK (Total Cases vs Total Deaths)
 SELECT Location, date, total_cases, total_deaths, (total_deaths/total_cases) * 100 AS Percentage
 	FROM [Covid Data SQL Project]..CovidDeaths$
 	WHERE Location='United Kingdom'
 	ORDER BY 1,2
 
--- Total Cases vs Population (% Total infections - UK)
+-- % Total infections - UK (Total Cases vs Population)
 SELECT Location, date, population, total_cases, (total_cases/population) * 100 AS Percentage
 	FROM [Covid Data SQL Project]..CovidDeaths$
 	WHERE Location='United Kingdom'
 	ORDER BY 1,2
 
--- Infection rate vs Population (Countries with worst infection rate)
+-- Countries with highest total infection rate (Infection rate vs Population)
 SELECT Location, population, MAX(total_cases) AS InfectionCount, MAX((total_cases/population)) * 100 AS Percentage
 	FROM [Covid Data SQL Project]..CovidDeaths$
 	WHERE continent IS NOT NULL
 	GROUP BY location, population
 	ORDER BY Percentage DESC
 
--- Death count by country
+-- Total death count by country
 SELECT Location, MAX(cast(total_deaths as int)) AS DeathCount
 	FROM [Covid Data SQL Project]..CovidDeaths$
 	WHERE continent IS NOT NULL
@@ -64,26 +65,25 @@ SELECT dea.continent, dea.location, dea.date, dea.population,vac.new_vaccination
 	WHERE dea.continent IS NOT NULL
 	ORDER BY 1,2,3
 
--- Rolling vaccinations by day per country
+-- Rolling vaccinations by day per country (CTE)
 
 With RollingVaccinations (Continent, Location, Date, Population, New_Vaccinations, TotalPeopleVaccinated)
-	as
-	(
+as
+(
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
-	SUM(CONVERT(bigint, vac.new_vaccinations)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS TotalPeopleVaccinated
-	FROM [Covid Data SQL Project]..CovidDeaths$ dea
-	JOIN [Covid Data SQL Project]..CovidVaccinations$ vac
-		ON dea.location = vac.location
-		AND dea.date = vac.date
-	WHERE dea.continent IS NOT NULL
-	-- ORDER BY 2,3
-	)
+SUM(CONVERT(bigint, vac.new_vaccinations)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS TotalPeopleVaccinated
+FROM [Covid Data SQL Project]..CovidDeaths$ dea
+JOIN [Covid Data SQL Project]..CovidVaccinations$ vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL
+)
 
-	SELECT *, (TotalPeopleVaccinated/Population)*100 AS PercentagePeopleVaccinated
-	FROM RollingVaccinations
+SELECT *, (TotalPeopleVaccinated/Population)*100 AS PercentagePeopleVaccinated
+FROM RollingVaccinations
 
 
--- Temp table
+-- Rolling vaccinations by day per country (Temp table)
 DROP TABLE IF exists PercentPopulationVaccinated
 CREATE TABLE PercentPopulationVaccinated
 (
@@ -97,18 +97,19 @@ TotalPeopleVaccinated numeric
 
 Insert into PercentPopulationVaccinated
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
-	SUM(CONVERT(bigint, vac.new_vaccinations)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS TotalPeopleVaccinated
-	FROM [Covid Data SQL Project]..CovidDeaths$ dea
-	JOIN [Covid Data SQL Project]..CovidVaccinations$ vac
-		ON dea.location = vac.location
-		AND dea.date = vac.date
-	WHERE dea.continent IS NOT NULL
-	-- ORDER BY 2,3
+SUM(CONVERT(bigint, vac.new_vaccinations)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS TotalPeopleVaccinated
+FROM [Covid Data SQL Project]..CovidDeaths$ dea
+JOIN [Covid Data SQL Project]..CovidVaccinations$ vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL
 
 SELECT *, (TotalPeopleVaccinated/Population)*100
 	FROM PercentPopulationVaccinated
 
+
 -- Create view to store data
+DROP VIEW PercentPopulationVaccinatedView 
 CREATE VIEW PercentPopulationVaccinatedView AS
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
 SUM(CONVERT(bigint, vac.new_vaccinations)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS TotalPeopleVaccinated
@@ -117,4 +118,3 @@ JOIN [Covid Data SQL Project]..CovidVaccinations$ vac
 	ON dea.location = vac.location
 	AND dea.date = vac.date
 WHERE dea.continent IS NOT NULL
--- ORDER BY 2,3
